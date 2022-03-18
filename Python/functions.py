c471004eb4705ed271e08c6db28d0738db36043a
netW3k <wiktorwolarz2003gmail.com>
2022-03-18 19:19:49 +0100

    Basic function added, stable

--- Python/functions.py
+++ Python/functions.py
@@ -1,22 +1,67 @@
+import traceback
 import requests
+import sys
+import random
+
 from difflib import get_close_matches as gcm
 
 
+__API1 = 'https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json'
+__API2 = 'https://store.steampowered.com/api/appdetails?appids='
+__WISHEDCURRENCY = 'nok'
+
+
+def __getJSON(api, appid = None):
+     # <IF SOMETHING GOES WRONG WITH THIS FUNCTION IT WILL RETURN 'NONETYPE' WHICH ESSENTIALY WILL MAKE EVERYTHING FALSE>
+     ## DEV NOTE ## Be careful of arguments/parameters placement. API is FIRST declared, and appid second. 
+
+
+     try:
+          if appid == None: return requests.get(api).json()
+          else: return requests.get(f"{api}{appid}&cc={__WISHEDCURRENCY}").json()
 
 
 
+     except Exception as e: 
+
+               print('######################################################') #TODO CREATE BETTER EXCEPTION MENAGAMNET 
+               print(f"\n ERROR OF PRIVATE FUNCTION - getJSON. RETRUNED AS 'NONE' \n {e} ")
+               print('######################################################')
+               #print(e)
+               #traceback.format_exc() #TODO CREATE LOG FILE IF EXCEPTION RAISES 
+               return None                        
+               
+
+def __isSuccess(gameID):
+     # <This is function is needed in order to prevent any 'None-transcipable' errors, and mainly to check if Steam database consider the enry as successfull>
+     
+     tempjs = __getJSON(__API2, gameID)
+
+     if tempjs is None: return False
+
+     try:
+          return tempjs[str(gameID)]['success'] 
+
+     except Exception as e:
+
+          print('######################################################')
+          print(f"ERROR OF PRIVATE FUNCTION - isSuccess. ERROR AS : ")
+          print('######################################################')
+          traceback.format_exc()
 
 
-def validate_game(game, suggestions = False,): #Returns a boolean value
+def validateGame(game, returnappID = False, suggestions = False,): #Returns a boolean value
 
      appID = None
      gamesuggestions = []
+
      game = game.lower()
 
-     print("Validatating " + game)
+     #print("Validatating " + game)
 
      try:
-          appjs = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json').json()
+          appjs = __getJSON(__API1)
+          if appjs is None: return 
 
           for item in appjs['applist']['apps']:
                
@@ -24,10 +69,9 @@ def validate_game(game, suggestions = False,): #Returns a boolean value
 
                     appID = item['appid']
 
-                    gamejs = requests.get(f'https://store.steampowered.com/api/appdetails?appids={appID}').json()
                     print(appID)
 
-                    return gamejs[str(appID)]['success'] #If the game exists and steam do have any record of the game, this should return True 
+                    return appID if returnappID else __isSuccess(appID) # < If the game exists and steam do have any record of the game, this should return True.  >
 
                gamesuggestions.append(item['name'])
 
@@ -36,12 +80,249 @@ def validate_game(game, suggestions = False,): #Returns a boolean value
 
           return gcm(game, gamesuggestions, 10, 0.6)
           
+     
+     except Exception as e:
+
+          print("######################################################")
+          print(f"ERROR OF PUBLIC FUNCTION validateGame! ERROR AS: ")
+          print("######################################################")
+          traceback.format_exc()
+          
+          
+          return None 
+
+
+def gameDesc(appID, option = 'short'):
+
+     try:
+          gamejs = __getJSON(__API2, appID)
+          
+          if not __isSuccess(appID): #WE NEED TO CHECK IF THE ENTRY IS CONSIDERED AS SUCCESSFULL IN STEAM API, IF NOT IT CAUSES ERROR
+               return None 
+
+          if option == 'short': #TODO <IN PYTHON 3.10 YOU HAVE MATCH-CASE FUNCTION, HOWEVER IT'S NOT SUPPORTED WITH UBUNTU 20.04 (AS OF 16 MARCH 2022)> 
+               
+               desc = gamejs[str(appID)]['data']['short_description']
+              
+               #long_desc = gamejs[str(appID)]['data']['detailed_description'] < RETURNED DATA IS WRITTEN IN SOME KIND OF MARKUP LANGUAGE AND IT'S IMPOSSIBLE AS OF NOW TO CLEAN THE OUTPUT, AND THERFORE IT'S COMMENTED OUT. MAYBE IT WILL BE IMPLEMENTED IN THE FUTURE RELEASE,BUT HIGHLY UNLIKELY>
+              
+               return desc
 
      except Exception as e:
-          print("#########################################################################################")
-          print(f"Error of validating! Error as: {e}")
-          print("#########################################################################################")
-          return None #None or null? #TODO 
+
+          print("###############################################")
+          print(f"ERROR OF PUBLIC FUNCTION gameDesc! ERROR AS: ")
+          print("###############################################")
+          traceback.format_exc()
+
+          return None 
 
 
+def releaseDate(gameID, csoon = False):
 
+     gamejs = __getJSON(__API2, gameID)
+     
+     try:
+          if csoon: return gamejs[str(gameID)]['data']['release_date']['coming_soon'] 
+          
+          return gamejs[str(gameID)]['data']['release_date']['date'] 
+
+     except Exception as e:
+
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - releaseDate. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+
+               return None 
+
+
+def isFree(gameID):
+
+     gamejs = __getJSON(__API2,gameID)
+
+     try: 
+          return gamejs[str(gameID)]['data']['is_free'] if gamejs is not None else False
+     
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - isFree. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def storePage(gameID):
+
+     try: return f"https://store.steampowered.com/app/{str(gameID)}/?cc={__WISHEDCURRENCY}"
+     
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - storePage. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def gamePrice(appID):
+
+     try: 
+
+          if isFree(appID): return 'The game is free!'
+
+          gamejs = __getJSON(__API2,appID)
+          currency = gamejs[str(appID)]['data']['price_overview']['currency']
+          price = gamejs[str(appID)]['data']['price_overview']['final_formatted']
+
+          return f"{price} {currency}"
+
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - gamePrice. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def gameDiscount(gameID, checkifgameisondiscount = False):
+
+     try:
+
+          if isFree(gameID) & checkifgameisondiscount == True: return False, 'FREE'
+          elif isFree(gameID) & checkifgameisondiscount == False: return 'No discount, the game is free to play!'
+
+          gamejs = __getJSON(__API2, gameID)
+          discount = gamejs[str(gameID)]['data']['price_overview']['discount_percent']
+
+          return True if checkifgameisondiscount & discount > 0 else f"{discount}%" 
+
+     except Exception as e: 
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - gameDiscount. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def randomGame(nameorid = 'id'):
+
+     try:
+          appjs = __getJSON(__API1)
+          isgame = False
+  
+          while not isgame:
+                    
+               random_entry = random.choice(appjs['applist']['apps'])
+               random_appID = str(random_entry['appid']) 
+                                        
+               try:
+                    
+                    if not __isSuccess(random_appID): continue
+                    
+               except : continue
+
+               gamejs = __getJSON(__API2, random_appID)
+
+               if gamejs[random_appID]['data']['type'] != 'game' : continue 
+               
+               isgame = True
+                    
+          return random_entry['name'] if nameorid == 'name' else random_appID
+                         
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - randomGame. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def randomFreeGame(nameorid = 'id'):
+     # <This function may take some time to proccesss>
+     try:
+          appjs = __getJSON(__API1)
+          isgame = False
+
+          while not isgame:
+
+               random_entry = random.choice(appjs['applist']['apps'])
+               random_appID = str(random_entry['appid'])
+
+               try:
+
+                    if not __isSuccess(random_appID): continue
+
+               except: continue
+
+               gamejs = __getJSON(__API2, random_appID)
+               
+               if not isFree(random_appID): continue
+               if gamejs[random_appID]['data']['type'] != 'game': continue
+
+               isgame = True
+          
+          return random_entry['name'] if nameorid == 'name' else random_appID
+
+     except Exception as e: 
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - randomFreeGame. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def gameHeaderImage(gameID):
+     # <NOTE: This function returns a link to the image, this means that you need to proccess the return value and show it on your own>
+     try: 
+          if not __isSuccess(gameID) : return None
+
+          gamejs = __getJSON(__API2, gameID)
+
+          return gamejs[str(gameID)]['data']['header_image']
+
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - gameHeaderImage. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+
+def gameDev(gameID):
+     # < Returns string as a connected elements of an array >
+
+     try:
+          if not __isSuccess(gameID) : return None
+
+          gamejs = __getJSON(__API2, gameID)
+
+          return ' , '.join(gamejs[str(gameID)]['data']['developers'])
+
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - gameDev. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+
+def gamePub(gameID):
+     # <  Returns string as a connected elements of an array >
+
+     try:
+          if not __isSuccess(gameID) : return None
+
+          gamejs = __getJSON(__API2, gameID)
+
+          return ' , '.join(gamejs[str(gameID)]['data']['publishers'])
+
+     except Exception as e:
+               print('######################################################')
+               print(f"ERROR OF PUBLIC FUNCTION - gamePub. ERROR AS : \n {traceback.format_exc()}")
+               print('######################################################')
+               return None
+          
+#game = 'scavengers'
+#testID = validateGame(game, returnappID=True)
+#if validateGame(game):
+#     print(f"Test: {gameDesc(testID)}")
+#else: print(f"Else test: {validateGame(game, suggestions=True)}")
+#print(storePage(testID))
+#print(gamePrice(testID))
+#print(gameDiscount(testID))
+
+game = randomGame('id')
+print(game)
+print(f"{gameDev(game)} -- {gamePub(game)}")
+
+#TODO Random buygame roullete? for showcase
