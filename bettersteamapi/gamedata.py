35d82a6480e552929150d17dc1fe42a2c1817b92
netW3k <wiktorwolarz2003gmail.com>
2022-05-06 18:07:45 +0200

    documentation of the functions

--- bettersteamapi/gamedata.py
+++ bettersteamapi/gamedata.py
@@ -12,7 +12,7 @@ def change_wished_currency(currency: str) -> None:
           Some of the supported currencies:
                'us' - USD ($) American Dollars,
 
-               'nok' - Norwegian Kroner, 
+               'nok' - Norwegian Krones, 
 
                'au' - AUD (A$) Australian Dollars,
 
@@ -25,11 +25,14 @@ def change_wished_currency(currency: str) -> None:
      
      """ 
      config.wished_country_currency = currency
+     #print(config.wished_country_currency)
 
          
 def get_wished_currency() -> str:
      """ Get the the currency from the config file.
-         If you want to change the current currency use the 'change_wished_currency' function.
+
+         If you want to change the current currency use 'change_wished_currency()' function
+
      Returns:
           String: The current currency that is set in config.json file
      """ 
@@ -40,8 +43,8 @@ def __get_json(gameid: str = 'None') -> dict:
      """This function requests steam API, converts it into a JSON and returns it
 
      Args:
-          gameid (str): If empty this function will request API with all id's associated with a gamename
-                        otherwise it will dict with all data of the game.
+          gameid (str): If empty this function will request API with all id's associated with a gamename,
+                        if id is provided it will return dict with data associated with the id.
 
      Returns:
           Dictionary: If args is empty -> dict with ids and names of all games in steam DB.
@@ -57,10 +60,15 @@ def __get_json(gameid: str = 'None') -> dict:
 
 
 def __is_success(gameid: str) -> bool:
-     """
-     Returns whatever value is under JSON 'success' key, if None it returns False. Used to check if a game is recognized as successfull in steam DB.
-     This function is mainly used to check if a game is True under 'success', if not, it means that something is wrong, and therfore it is used as a
-     prevention function (if returns False, everything else will return False or None).
+     """Checks if the call is successful and if there is data avaiable.
+
+     Args: 
+          gameid(str): ID of the game
+
+     Returns: 
+          Boolean:  True -> the call is successful and listings are avaiable
+                    False -> the call is unsuccessful, and something went wrong or there are no listing
+                             for this ID.
      """
      # <This is function is needed in order to prevent any 'None-transcipable' errors, and mainly to check if Steam database consider the entry as successfull>
 
@@ -69,27 +77,37 @@ def __is_success(gameid: str) -> bool:
      return game_json[gameid]["success"]
 
 
-def validate_game(game: str, return_gameid: bool = False):
-     """Validates if provided game exists in steam DB.
+def validate_game(game_name: str, return_gameid: bool = False):
+     """Validates if the game exists in DB, and has an option to return ID of specified game if 2nd arg is set to True.
 
-     :returns: returns a boolean value based on if the game was found or not, if return_gameID is set to False, otherwise it will return gameID
+     Args:
+          game_name(str): name of the game you want to validate
+          return_gameid(boolean): if you want to return ID of the game. (If game not found it will return 'None')
+
+     Returns: 
+          return_gameid: False -> returns bool based on if the game exist in steam DB or not
+                         True -> returns ID of the game as string
      """
      
-     game = game.lower()
+     game_name = game_name.lower()
      idjson = __get_json()
 
      for item in idjson["applist"]["apps"]:
-        if item["name"].lower() == game and __is_success(str(item['appid'])):
+        if item["name"].lower() == game_name and __is_success(str(item['appid'])):
             gameid = str(item["appid"]) 
             return gameid if return_gameid else True # <If the game exists and steam do have any record of the game, this should return True.>
 
      return False
 
+
 def game_suggestions(game: str) -> list[str]:
-     """ Returns suggestions based on game value.
+     """ Get suggestions close to the specified game
+
+     Args:
+          game(str): name of the game you want to reference from 
 
      Returns: 
-          List: Of strings that are similair to 'game' argument
+          List: returns a list(str)
      """
 
      game = game.lower()
@@ -100,16 +118,23 @@ def game_suggestions(game: str) -> list[str]:
 
 
 def get_game_description(gameid: str) -> str:
+     ''' Get short description of the game
+     '''
      gamejson = __get_json(gameid)
      return gamejson[gameid]["data"]["short_description"]
 
 
 def get_release_date(gameid: str, return_comming_soon: bool = False) -> str:
-     """Get the release date of the provided game.
+     """Get the release date of the provided game. Optional: return bool based on if the game is comming soon or not.
      
      Args:
-      Return_coming_soon: if True, it checks and returns if a game officialy 
-      released or is releasing soon on platform as bool.
+          gameid(str): ID of the game
+          return_comming_soon(bool): if set to True, checks and returns if a game is officialy released 
+                               or is releasing soon on platform.
+     
+     Returns:
+          return_comming_soon: True -> returns bool based on if the game is comming soon.
+                               False -> returns the release date of the game
      """
 
      gamejson = __get_json(gameid)
@@ -126,15 +151,14 @@ def check_if_free(gameid: str) -> bool:
 
 
 def get_store_page(gameid: str) -> str:
+     ''' Get the steam store pageof the game
+
+     Returns a link to the storepage.
+     '''
      return f"https://store.steampowered.com/app/{gameid}/?cc={get_wished_currency()}"
 
 
 def get_game_price(gameid: str) -> str:
-     """This function sometimes returns other currencies than expected. This most likely has to do with servers and which one you request from
-
-     Returns: 
-          String: Price and name of the currency. This is the final price, which means that if a game is on discount, this price will change accordingly.
-     """
     
      if check_if_free(gameid): return "The game is free!"
 
@@ -152,10 +176,10 @@ def get_game_price(gameid: str) -> str:
 
 
 def get_game_discount(gameid: str,) -> str:
-     """ Gets the discount of the game.
+     """ Get the discount of the game.
 
      Returns:
-          String: The discount value in percent. 
+          String -> Returns the discount value in percent. 
      """
      gamejson = __get_json(gameid)
      discount = str(gamejson[str(gameid)]["data"]["price_overview"]["discount_percent"])
@@ -167,7 +191,9 @@ def check_if_game_on_discount(gameid: str) -> bool:
      """ Check if the provided game is on discount.
 
      Raises:
-          KeyError: One of the keys are not present in the JSON file of the provided game.
+          KeyError: One of the keys are not present in the JSON file of the provided game. 
+                    Is raised when JSON key is missing. If the game is free to play it can cause 
+                    this error to raise. 
      
      Returns:
           Boolean: True if the game is on discount (not 0), False if the game is not on discount (0)
@@ -180,14 +206,14 @@ def check_if_game_on_discount(gameid: str) -> bool:
           discount = gamejson[str(gameid)]["data"]["price_overview"]["discount_percent"]
 
      except KeyError: 
-          print('Could not find the specified key in the JSON file')
+          print('The price value key is not listed in JSON file. Check if provided game is not free-to-play')
           return False
 
      return True if discount > 0 else False
 
 
 def get_random_game() -> dict[str, str]:
-     """Random game from steam applist, checks if it is a game and returns dict with name and id.
+     """Get random game from steam applist, checks if it is a game and returns dict with name and id.
         
         Keys: 'name' | 'appid'
      """
@@ -207,20 +233,24 @@ def get_random_game() -> dict[str, str]:
           if gamejson[random_appid]['data']['type'] != 'game': continue
 
           is_game = True
-          random_game = {"name" : random_entry['name'], "appid" : str(random_entry['appid'])} 
-          # needed in order to change id from integer to a string data type
+          random_game = {"name" : random_entry['name'], "appid" : str(random_entry['appid'])} # needed in order to change id from integer to a string data type
+          
      
      return random_game
      
 
 def random_free_game() -> dict[str, str]:
-     """Random free game from steam applist, checks if it is a game and returns dict with name and id.
+     """Get random free game from steam applist, checks if it is a game and returns dict with name and id.
+        
+        NOTE -> This function can take some time before returning a value.
         
         Keys: 'name' | 'appid'
      """
 
-     # <This function may take some time to proccess>
-     # TODO Create possibilty to generate a JSON file of all free games with an update function
+     # TODO Create possibilty to generate a JSON file of all free games with an update function. Create a new module 
+     # for this?
+
+     # <This function may take some time to proccess and return a value>
 
      idjson = __get_json()
      is_game = False
@@ -247,9 +277,11 @@ def random_free_game() -> dict[str, str]:
 
 def game_header_image(gameid: str) -> str:
      """Get the header image of the game.
-     :returns: Link as a string to the image.
+     
+     Returns: 
+          String -> link to the header image. Useful if frontend automatically gets the image by providing link to the 
+                    image.
      """
-     # <NOTE: This function returns a link to the image, this means that you need to proccess the return value and show it on your own>
 
      gamejson = __get_json(gameid)
 
@@ -261,6 +293,7 @@ def game_developers(gameID: str) -> list[str]:
 
      Raises:
           KeyError: One of the keys are not present in the JSON file of the provided game.
+                    KeyError returns empty list.
      """
 
      game_JSON = __get_json(gameID)
@@ -269,17 +302,25 @@ def game_developers(gameID: str) -> list[str]:
           return game_JSON[gameID]['data']['developers']
 
      except KeyError:
-          print("Could not find the specified key in JSON file.")
+          print("Could not find the 'developers' key in the JSON file/request.")
           return []
 
 def game_publishers(gameid: str) -> list[str]:
      """Get a list of game publishers.
+
+     Raises:
+          KeyError: One of the keys are not present in the JSON file of the provided game.
+                    KeyError returns empty list.
      """
 
      gamejson = __get_json(gameid)
+     
+     try:
+          return gamejson[gameid]['data']['publishers']
 
-     return gamejson[gameid]['data']['publishers']
-
+     except KeyError:
+          print("Could not find the 'publishers' key in the JSON file/request")
+          return []
 
 def game_genres(gameid: str) -> list[str]:
      """Get a list of game generes.
